d864a3f5c5ab7c142c8b6721e2b69a37daed488c
krypto5863 <29824718+krypto5863@users.noreply.github.com>
2021-10-25 14:27:07 -0400

    Fixed issue where players would be missing folders and games would crash.

--- ExtendedErrorHandling/Main.cs
+++ ExtendedErrorHandling/Main.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Security;
 using System.Security.Permissions;
 using System.Text;
+using System.Windows.Forms;
 using UnityEngine.SceneManagement;
 
 [module: UnverifiableCode]
@@ -15,7 +16,7 @@ using UnityEngine.SceneManagement;
 
 namespace ExtendedErrorHandling
 {
-	[BepInPlugin("ExtendedErrorHandling", "ExtendedErrorHandling", "1.2")]
+	[BepInPlugin("ExtendedErrorHandling", "ExtendedErrorHandling", "1.3")]
 	public class Main : BaseUnityPlugin
 	{
 
@@ -26,12 +27,34 @@ namespace ExtendedErrorHandling
 			BepLogger = Logger;
 			main = this;
 
+			CreateMissingFolders();
+
 			Harmony.CreateAndPatchAll(typeof(MenuItemRedundancy));
 			Harmony.CreateAndPatchAll(typeof(PresetErrorHandling));
 			Harmony.CreateAndPatchAll(typeof(Serialization));
 			Harmony.CreateAndPatchAll(typeof(ErrorTexturePlaceholder));
 
 			UnityEngine.SceneManagement.SceneManager.sceneLoaded += MenuItemRedundancy.DoOnTitleScreen;
+		}
+
+		public static void CreateMissingFolders() 
+		{
+			var dirs = new string[]{ "Mod", "SaveData", "Preset", "MyRoom", "PhotoModeData", "ScreenShot", "Thumb" };
+
+			foreach (string s in dirs)
+			{
+				if (!Directory.Exists(BepInEx.Paths.GameRootPath + $"\\{s}")) 
+				{
+					try
+					{
+						Directory.CreateDirectory(BepInEx.Paths.GameRootPath + $"\\{s}");
+					}
+					catch 
+					{
+						BepLogger.LogFatal($"We couldn't create the directory {BepInEx.Paths.GameRootPath}\\{s}. Please create it manually or you will have errors.");					
+					}
+				}		
+			}			
 		}
 	}
 }
