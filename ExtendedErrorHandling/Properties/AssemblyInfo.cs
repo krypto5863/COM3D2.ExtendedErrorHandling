db1193431350a4624ef5c97d5936196b32ffd7ae
krypto5863 <29824718+krypto5863@users.noreply.github.com>
2024-04-30 17:17:47 -0400

    Ugh

--- ExtendedErrorHandling/Properties/AssemblyInfo.cs
+++ ExtendedErrorHandling/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Instance")]
+[assembly: AssemblyTitle("ExtendedErrorHandling")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Instance")]
+[assembly: AssemblyProduct("ExtendedErrorHandling")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
